8d95a863ab0d8a1eb922f5b06f031895f7e29d72
David Steinberg <david@resium.com>
2017-08-01 18:33:53 -0700

    Switch to popsicle for requests

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 // This file allows us to describe each protocols controller methods in their
 // own module.
-var request = require('request');
+const request = require('popsicle')
 
 var URL = "http://localhost:8080";
 
 function list_samples(connection, callback) {
   // sample_ids
-  return request.get(URL + "/list_samples", callback);
+  return request.get({url: URL + "/list_samples"})
+      .use(request.plugins.parse('json'))
+      .then(callback);
 }
 
 function list_features(connection, callback) {
   // feature_ids
-  return request.get(URL + "/list_features", callback);
+  return request.get({url: URL + "/list_features"})
+    .use(request.plugins.parse('json'))
+    .then(callback);;
 }
 
 function matrix(connection, sample_ids, feature_ids, callback) {
   var payload = {sample_ids: sample_ids, feature_ids: feature_ids};
   // matrix
   return request.post(
-    {url: URL + "/matrix",json: true, body: payload}, callback);
+    {url: URL + "/matrix",json: true, body: payload})
+      .use(request.plugins.parse('json'))
+      .then(callback);;
 }
 
 function sparse_matrix(connection, sample_ids, feature_ids, callback) {
   var payload = {sample_ids: sample_ids, feature_ids: feature_ids};
   // matrix
   return request.post(
-    {url: URL + "/matrix/sparse",json: true, body: payload}, callback);
+    {url: URL + "/matrix/sparse",json: true, body: payload})
+      .use(request.plugins.parse('json'))
+      .then(callback);;
 }
 
 function sparse_to_dense(sparse, dense) {
